221abb9404d66c84896ef65610c2a9f7d4cd6b4a
jrippon27 <jrippon27@gmail.com>
2016-11-02 11:12:23 -0700

    calculates area and perimeter

---
+++ area_parameter_calculator/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace area_parameter_calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstNumber = new System.Windows.Forms.TextBox();
+            this.secondNumber = new System.Windows.Forms.TextBox();
+            this.thirdNumber = new System.Windows.Forms.TextBox();
+            this.btnArea = new System.Windows.Forms.Button();
+            this.btnPerimeter = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // firstNumber
+            // 
+            this.firstNumber.Location = new System.Drawing.Point(68, 76);
+            this.firstNumber.Name = "firstNumber";
+            this.firstNumber.Size = new System.Drawing.Size(100, 20);
+            this.firstNumber.TabIndex = 0;
+            // 
+            // secondNumber
+            // 
+            this.secondNumber.Location = new System.Drawing.Point(240, 76);
+            this.secondNumber.Name = "secondNumber";
+            this.secondNumber.Size = new System.Drawing.Size(100, 20);
+            this.secondNumber.TabIndex = 1;
+            // 
+            // thirdNumber
+            // 
+            this.thirdNumber.Location = new System.Drawing.Point(68, 169);
+            this.thirdNumber.Name = "thirdNumber";
+            this.thirdNumber.Size = new System.Drawing.Size(100, 20);
+            this.thirdNumber.TabIndex = 2;
+            // 
+            // btnArea
+            // 
+            this.btnArea.Location = new System.Drawing.Point(68, 248);
+            this.btnArea.Name = "btnArea";
+            this.btnArea.Size = new System.Drawing.Size(75, 23);
+            this.btnArea.TabIndex = 3;
+            this.btnArea.Text = "Area";
+            this.btnArea.UseVisualStyleBackColor = true;
+            this.btnArea.Click += new System.EventHandler(this.btnArea_Click);
+            // 
+            // btnPerimeter
+            // 
+            this.btnPerimeter.Location = new System.Drawing.Point(225, 248);
+            this.btnPerimeter.Name = "btnPerimeter";
+            this.btnPerimeter.Size = new System.Drawing.Size(75, 23);
+            this.btnPerimeter.TabIndex = 4;
+            this.btnPerimeter.Text = "Perimeter";
+            this.btnPerimeter.UseVisualStyleBackColor = true;
+            this.btnPerimeter.Click += new System.EventHandler(this.btnPerimeter_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "#1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(197, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "#2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "#3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(335, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "For Area what ever number not used enter \"1\". For Perimeter use \"0\".";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 348);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnPerimeter);
+            this.Controls.Add(this.btnArea);
+            this.Controls.Add(this.thirdNumber);
+            this.Controls.Add(this.secondNumber);
+            this.Controls.Add(this.firstNumber);
+            this.Name = "Form1";
+            this.Text = "Area and Perimeter Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox firstNumber;
+        private System.Windows.Forms.TextBox secondNumber;
+        private System.Windows.Forms.TextBox thirdNumber;
+        private System.Windows.Forms.Button btnArea;
+        private System.Windows.Forms.Button btnPerimeter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ area_parameter_calculator/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace area_parameter_calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnArea_Click(object sender, EventArgs e)
+        {
+            double firstTextBoxNumber;
+            double secondTextBoxNumber;
+            double thirdTextBoxNumber;
+            double answer;
+            
+
+            firstTextBoxNumber = double.Parse(firstNumber.Text);
+            secondTextBoxNumber = double.Parse(secondNumber.Text);
+            thirdTextBoxNumber = double.Parse(thirdNumber.Text);
+
+            answer = (firstTextBoxNumber * secondTextBoxNumber) *
+                thirdTextBoxNumber;
+            MessageBox.Show(answer.ToString());
+        }
+
+        private void btnPerimeter_Click(object sender, EventArgs e)
+        {
+            int firstTextBoxNumber;
+            int secondTextBoxNumber;
+            int thirdTextBoxNumber;
+            int answer;
+
+            firstTextBoxNumber = int.Parse(firstNumber.Text);
+            secondTextBoxNumber = int.Parse(secondNumber.Text);
+            thirdTextBoxNumber = int.Parse(thirdNumber.Text);
+
+            answer = firstTextBoxNumber + secondTextBoxNumber +
+                thirdTextBoxNumber;
+            MessageBox.Show(answer.ToString());
+        }
+    }
+}
