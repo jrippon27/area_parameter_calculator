0a0317e18f7ec2f3ac3f40e36e4269b97d757d64
James Rippon <jrippon27@gmail.com>
2017-06-02 15:36:34 -0700

    Area and Perimeter Calculator 1.1
    - Adds the button Clear to clear each textbox. 
    - Provides a message asking the user to enter either a 0 or a 1, if the third number is null or empty.
    - Removed instructions from the top of the program.

---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace area_parameter_calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnArea_Click(object sender, EventArgs e)
+        {
+            double firstTextBoxNumber;
+            double secondTextBoxNumber;
+            double thirdTextBoxNumber;
+            double answer;
+
+            
+            if (string.IsNullOrEmpty(thirdNumber.Text))
+            {
+                MessageBox.Show("Please enter a value of 1 as the third number cannot be empty!");
+            }
+            else
+            {
+                firstTextBoxNumber = double.Parse(firstNumber.Text);
+                secondTextBoxNumber = double.Parse(secondNumber.Text);
+                thirdTextBoxNumber = double.Parse(thirdNumber.Text);
+
+                answer = (firstTextBoxNumber * secondTextBoxNumber) *
+                    thirdTextBoxNumber;
+                MessageBox.Show(answer.ToString());
+            }
+        }
+
+        private void btnPerimeter_Click(object sender, EventArgs e)
+        {
+            int firstTextBoxNumber;
+            int secondTextBoxNumber;
+            int thirdTextBoxNumber;
+            int answer;
+
+            if (string.IsNullOrEmpty(thirdNumber.Text))
+            {
+                MessageBox.Show("Please enter a value of 0 as the third number cannot be empty!");
+            }
+            else
+            {
+                firstTextBoxNumber = int.Parse(firstNumber.Text);
+                secondTextBoxNumber = int.Parse(secondNumber.Text);
+                thirdTextBoxNumber = int.Parse(thirdNumber.Text);
+
+                answer = firstTextBoxNumber + secondTextBoxNumber +
+                    thirdTextBoxNumber;
+                MessageBox.Show(answer.ToString());
+            }
+        }
+        private void firstNumber_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            firstNumber.Clear();
+            secondNumber.Clear();
+            thirdNumber.Clear();
+        }
+    }
+}
